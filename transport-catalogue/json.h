15047ed14773704584546a933cc568bacf66f8cc
Kolmagorov Alexey <3285297@mail.ru>
2024-07-23 00:16:22 +0500

    Внесены исправления

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -16,46 +16,35 @@ using Dict = std::map<std::string, Node>;
 using Array = std::vector<Node>;
  
 class ParsingError : public std::runtime_error {
-public:
+   public:
     using runtime_error::runtime_error;
 };
- 
-class Node {
-public:
- 
-    using Value = std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string>;
- 
-    Node() = default;
-    Node(bool value);
-    Node(Array array);
-    Node(Dict map);
-    Node(int value);
-    Node(std::string value);
-    Node(std::nullptr_t);
-    Node(double value);
-    
+
+class Node final : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> {
+   public:
+
+    using variant::variant;
+    using Value = variant;
+
     const Array& AsArray() const;
     const Dict& AsMap() const;
     int AsInt() const;
     double AsDouble() const;
     bool AsBool() const;
     const std::string& AsString() const;
- 
     bool IsNull() const;
-    bool is_int() const;
+    bool IsInt() const;
     bool IsDouble() const;
     bool IsRealDouble() const;
     bool IsBool() const;
     bool IsString() const;
     bool IsArray() const;
     bool IsMap() const;
- 
     const Value& GetValue() const;
-        
-private:
-    Value value_;
+
 };
- 
+
+
 inline bool operator==(const Node& lhs, const Node& rhs) { 
     return lhs.GetValue() == rhs.GetValue();
 }  
